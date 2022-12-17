d7c8f2ec07a4b53e24c62ca0a334ae0d4ba11649
Charles Gleason <36055314+crgl@users.noreply.github.com>
2022-12-17 12:46:29 -0500

    Add d9p2 solution

--- d9/solve.py
+++ d9/solve.py
@@ -26,6 +26,48 @@ def parse_input():
 def treecomp(df):
     return df.cummax().shift(1).fillna(-1)
 
+def drag(hpos, tpos):
+    hy, hx = hpos
+    ty, tx = tpos
+    if ty == hy:
+        if hx - tx > 1:
+            tx += 1
+        elif hx - tx < -1:
+            tx -= 1
+    elif tx == hx:
+        if hy - ty > 1:
+            ty += 1
+        elif hy - ty < -1:
+            ty -= 1
+    else:
+        if hy - ty > 1:
+            ty += 1
+            if hx > tx:
+                tx += 1
+            else:
+                tx -= 1
+        elif hy - ty < -1:
+            ty -= 1
+            if hx > tx:
+                tx += 1
+            else:
+                tx -= 1
+        elif hx - tx > 1:
+            tx += 1
+            if hy > ty:
+                ty += 1
+            else:
+                ty -= 1
+        elif hx - tx < -1:
+            tx -= 1
+            if hy > ty:
+                ty += 1
+            else:
+                ty -= 1
+    tpos = (ty, tx)
+    hpos = (hy, hx)
+    return (hpos, tpos)
+
 def p1(moves):
     hpos = (0, 0)
     tpos = (0, 0)
@@ -36,50 +78,30 @@ def p1(moves):
         hy, hx = hpos
         hy += dy
         hx += dx
-        ty, tx = tpos
-        if ty == hy:
-            if hx - tx > 1:
-                tx += 1
-            elif hx - tx < -1:
-                tx -= 1
-        elif tx == hx:
-            if hy - ty > 1:
-                ty += 1
-            elif hy - ty < -1:
-                ty -= 1
-        else:
-            if hy - ty > 1:
-                ty += 1
-                if hx > tx:
-                    tx += 1
-                else:
-                    tx -= 1
-            elif hy - ty < -1:
-                ty -= 1
-                if hx > tx:
-                    tx += 1
-                else:
-                    tx -= 1
-            elif hx - tx > 1:
-                tx += 1
-                if hy > ty:
-                    ty += 1
-                else:
-                    ty -= 1
-            elif hx - tx < -1:
-                tx -= 1
-                if hy > ty:
-                    ty += 1
-                else:
-                    ty -= 1
-        tpos = (ty, tx)
         hpos = (hy, hx)
+        hpos, tpos = drag(hpos, tpos)
         tpos_rec.add(tpos)
     print("P1:", len(tpos_rec))
 
 
 def p2(moves):
-    print("P2:")
+    positions = [(0, 0)] * 10
+    tpos_rec = set()
+    tpos_rec.add(positions[-1])
+    for move in moves:
+        dy, dx = move
+        hy, hx = positions[0]
+        hy += dy
+        hx += dx
+        positions[0] = (hy, hx)
+        for i in range(9):
+            hpos = positions[i]
+            tpos = positions[i + 1]
+            hpos, tpos = drag(hpos, tpos)
+            positions[i] = hpos
+            positions[i + 1] = tpos
+        tpos_rec.add(positions[-1])
+    print("P2:", len(tpos_rec))
 
 
 if __name__ == '__main__':
