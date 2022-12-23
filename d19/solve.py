ab41572cf4cef54f6e1cb2304543591f259c41a8
Charles Gleason <36055314+crgl@users.noreply.github.com>
2022-12-23 09:17:36 -0500

    Add skeletons for d16-d21 scripts

---
+++ d19/solve.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from copy import deepcopy
+from time import time
+
+def parse_input():
+    with open('input.txt', 'r') as f:
+        for line in f.readlines():
+            pass
+    return None
+
+def p1():
+    print("P1:")
+
+def p2():
+    print("P2:")
+
+if __name__ == '__main__':
+    data = parse_input()
+    p1()
+    p2()
